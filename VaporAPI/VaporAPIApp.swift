ac0b62a552e6ff488a832872050e1d6b241aa75d
pieterbikkel <pieterbikkel@gmail.com>
2022-06-12 22:26:27 +0200

    Initial Commit

---
+++ VaporAPI/VaporAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaporAPIApp.swift
+//  VaporAPI
+//
+//  Created by Pieter Bikkel on 12/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct VaporAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
